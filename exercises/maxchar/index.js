35e6af5b079fd8c08bdbd15abb653359c1d83cfb
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-01 16:42:16 +0300

    maxChar ok

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,42 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//my solution
+function maxChar(str) {
+    let chars = {};
+
+    for (let char of str) {
+        chars[char] = chars[char] + 1 || 1;
+    }
+
+    let desc = Object.entries(chars).sort((a, b) => b - a);
+
+    return desc[0][0];
+}
+
+//Stephen's solution
+// function maxChar(str) {
+//     const charMap = {};
+//     let max = 0;
+//     let maxChar = '';
+
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;
+//         }
+//         else {
+//             charMap[char] = 1;
+//         }
+//     }
+
+//     for (let char in charMap) {
+//         if (charMap[char] > max) {
+//             max = charMap[char];
+//             maxChar = char;
+//         }
+//     }
+
+//     return maxChar;
+// }
 
 module.exports = maxChar;
