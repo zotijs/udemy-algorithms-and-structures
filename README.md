3ea3bd25a8dcf19327652c013dbe08bf7c9f0571
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-01 14:17:08 +0300

    reversestring ok

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Algorithms + Data Structures Udemy Course
 
-Course material of Stephen Grider's udemy [course](https://www.udemy.com/coding-interview-bootcamp-algorithms-and-data-structure/)
+Course material from Stephen Grider's udemy [course](https://www.udemy.com/coding-interview-bootcamp-algorithms-and-data-structure/)
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//--- Debug:
+//place a debugger inside the function
+//comment out the function call below
+//reverse('abcd');
+//and finaly cd to the reversestring 'cd reversestring/' directory and run 'node inspect index.js'
+//type c or continue to continue the execution inside the terminal
+//type repl to debug the function from within the terminal
+
+//my solution
+// function reverse(str) {
+//     const strArray = str.split('').reverse();
+
+//     return strArray.join('');
+// }
+
+//Stephen's solution #1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+//Stephen's solution #2
+// function reverse(str) {
+//     let reversed = '';
+    
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+//Stephen's solution #3
+function reverse(str) {
+    return str.split('').reduce((reversed, character) => character + reversed, '');
+}
 
 module.exports = reverse;
