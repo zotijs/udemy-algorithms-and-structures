e995767bd8f17323e7a5db01ee3ccc71be63080c
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-02 12:19:25 +0300

    vowels ok

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+//stephen's solution #1
+function matrix(n) {
+    const results = [];
+
+    //initiate inner arrays
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+    }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n -1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        //responsible for the top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startColumn][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        //right column
+        for (let i = startRow; i<= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        //bottom row
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        //start column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+
+    return results;
+}
 
 module.exports = matrix;
