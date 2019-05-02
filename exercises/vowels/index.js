e47a138579201a7ebe5512787689c8c19302f5d0
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-02 11:46:45 +0300

    vowels ok

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//my solution
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+//stephens solution #1
+// function vowels(str) {
+//     let count = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count++;
+//         }
+//     }
+    
+//     return count;
+// }
+
+//stephen's solution #2
+//same as my solution
 
 module.exports = vowels;
