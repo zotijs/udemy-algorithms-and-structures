a0a5dfca243455b69024a0df9e080815ff4d9d1d
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-05 22:03:19 +0300

    selection, merge sort ok

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,15 +16,50 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    let minIndex = 0;
 
+    for (let i = 0; i < arr.length; i++) { 
+        minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            let lesser = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+    const results = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
