d6b32c14db79689e1522b3a991dd3137113d193d
Sotiris Tsiamouris <tsot85@gmail.com>
2019-05-05 17:39:05 +0300

    events ok

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -3,17 +3,65 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
+//my solution
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    const arr = this.events[eventName] ? [...this.events[eventName], callback] : [callback];
+    this.events = { ...this.events, [eventName]: arr };
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (!this.events[eventName]) {
+      return;
+    }
+
+    this.events[eventName].map(event => {
+      event();
+    });
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
+
+//stephen's solution
+// class Events {
+//   constructor() {
+//     this.events = {};
+//   }
+//   // Register an event handler
+//   on(eventName, callback) {
+//     if (this.events[eventName]) {
+//       this.events[eventName].push(callback);
+//     } else {
+//       this.events[eventName] = [callback];
+//     }
+//   }
+
+//   // Trigger all callbacks associated
+//   // with a given eventName
+//   trigger(eventName) {
+//     if (this.events[eventName]) {
+//       for (let cb of this.events[eventName]){
+//         cb();
+//       }
+//     }
+//   }
+
+//   // Remove all event handlers associated
+//   // with the given eventName
+//   off(eventName) {
+//     delete this.events[eventName];
+//   }
+// }
 
 module.exports = Events;
